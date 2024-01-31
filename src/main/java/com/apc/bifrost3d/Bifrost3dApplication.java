f0f19eeb3d45149b7ab49b1dec5a8653dde44741
Agustin Curutchet <agucuru@gmail.com>
2024-01-30 23:23:44 -0300

    first commit

---
+++ src/main/java/com/apc/bifrost3d/Bifrost3dApplication.java
@@ -0,0 +1,13 @@
+package com.apc.bifrost3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bifrost3dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bifrost3dApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apc/bifrost3d/Bifrost3dApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apc.bifrost3d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bifrost3dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
